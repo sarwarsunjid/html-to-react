38dc61b9d3b8e313272e808d1d9a1fb6d7a17284
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-05-17 15:58:13 +0600

    Use of Switch, Route for linkup Other Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+//import css from node
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import Home from './Home'
 import About from './About'
 import Service from './Service'
 import Contact from './Contact'
 
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 const App = () => {
   return (
     <>
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/about' component={About} />
-      <Route exact path='/service' component={Service} />
-      <Route exact path='/contact' component={Contact} />
-    </Switch>
-     
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/service' component={Service} />
+        <Route exact path='/contact' component={Contact} />
+        <Redirect to="/" />    
+      </Switch>
     </>
   )
 }
