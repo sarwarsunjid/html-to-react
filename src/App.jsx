a3f69416f2527afae54a29d77feb9f4fc5103112
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-05-17 19:04:21 +0600

    Creating Navbar Component, Check Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 //import css from node
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import './index.css';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
 import Home from './Home'
 import About from './About'
 import Service from './Service'
 import Contact from './Contact'
+import Navbar from "./Navbar"
 
 import {Switch, Route, Redirect} from 'react-router-dom';
 const App = () => {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/about' component={About} />
@@ -17,6 +21,7 @@ const App = () => {
         <Route exact path='/contact' component={Contact} />
         <Redirect to="/" />    
       </Switch>
+    
     </>
   )
 }
