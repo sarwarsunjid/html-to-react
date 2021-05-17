44fb3dbb0a994fd5124a30e9e9609b35a3d90454
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-05-17 13:43:20 +0600

    Use Switch Route for others component, Test Contact, Home, Service, About Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Home from './Home'
-//import {Switch, Route} from 'react-router-dom';
+import About from './About'
+import Service from './Service'
+import Contact from './Contact'
 
+import {Switch, Route} from 'react-router-dom';
 const App = () => {
   return (
     <>
-     <Home />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/service' component={Service} />
+      <Route exact path='/contact' component={Contact} />
+    </Switch>
+     
     </>
   )
 }
