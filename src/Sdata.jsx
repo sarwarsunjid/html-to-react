01cbc99c0a3a1802f43351c7539676496c47d809
Sarwar Jahan Bhuiyan <31752675+sunjid@users.noreply.github.com>
2021-05-20 13:29:13 +0600

    Service Page Component Updated,  Used Props to Pass Component

---
+++ src/Sdata.jsx
@@ -0,0 +1,36 @@
+import web from './images/web.jpg'
+import app from './images/app.jpg'
+import android from './images/android.jpg'
+import digital from './images/digital.jpg'
+import marketting from './images/marketting.jpg'
+import software from './images/software.jpg'
+
+const Sdata = [
+
+    {
+        imgsrc: web,
+        title: "web Development",
+    },
+    {
+        imgsrc: app,
+        title: "App Development",
+    },
+    {
+        imgsrc: android,
+        title: "Android Development",
+    },
+    {
+        imgsrc: digital,
+        title: "Digital",
+    },
+    {
+        imgsrc: marketting,
+        title: "Marketting",
+    },
+    {
+        imgsrc: software,
+        title: "Software Development",
+    },
+];
+
+export default Sdata;
--- src/Service.jsx
+++ src/Service.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import web from '../src/images/home-bg.jpg'
+// import web from '../src/images/home-bg.jpg'
+import Sdata from './Sdata'
+import Card from './Card'
 
 const Service = () => {
   return (
@@ -11,66 +13,15 @@ const Service = () => {
         <div className="row">
           <div className="col-10 mx-auto">
             <div className="row gy-4">
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={web} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={web} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={web} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={web} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={web} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-4 col-10 mx-auto">
-                <div class="card">
-                  <img src={web} class="card-img-top" alt="..." />
-                  <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                  </div>
-                </div>
-              </div>
+              {
+                Sdata.map((val, ind) => {
+                  return <Card
+                    key={ind}
+                    imgsrc={val.imgsrc}
+                    title={val.title}
+                  />
+                })
+              }
             </div>
           </div>
         </div>
